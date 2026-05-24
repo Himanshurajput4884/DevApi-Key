4db08849a2980301ce3a538e89e7471e953a86d6
Himanshu Kumar <himanshukumar@Himanshus-MacBook-Air.local>
2026-05-24 15:49:49 +0530

    Fixing issues

--- src/main/java/com/himanshu/kumar/LaughApi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/himanshu/kumar/LaughApi/configuration/OpenApiConfiguration.java
@@ -17,8 +17,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfiguration {
     private final OpenApiConfigurationProperties openApiConfigurationProperties;
 
-    private static final String BEARER_AUTH_COMPONENT_NAME = "Bearer Authentication";
-    private static final String BEARER_AUTH_SCHEME = "Bearer";
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
     @Bean
     public OpenAPI openAPI() {
@@ -31,10 +30,12 @@ public class OpenApiConfiguration {
         return new OpenAPI()
                 .info(info)
                 .components(new Components()
-                        .addSecuritySchemes(BEARER_AUTH_COMPONENT_NAME,
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
                                 new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
                                         .type(SecurityScheme.Type.HTTP)
-                                        .scheme(BEARER_AUTH_SCHEME)))
-                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH_COMPONENT_NAME));
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
     }
 }
